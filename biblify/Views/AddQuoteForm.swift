e7c66185926ece09054e0178cb9cb40cd679b583
Michał Talaga <michal.talaga.programming@gmail.com>
2024-01-28 22:37:49 +0100

    update, działający addQuoteForm, Popraawki w allQuoteView, Drobne poprawki contentView, Działający randomQuote

---
+++ biblify/Views/AddQuoteForm.swift
@@ -0,0 +1,18 @@
+//
+//  AddQuoteForm.swift
+//  biblify
+//
+//  Created by Michał Talaga on 28/01/2024.
+//
+
+import SwiftUI
+
+struct AddQuoteForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddQuoteForm()
+}
